1f3315819341abd67b71a0f817fb330c63eb24d8
michaelramoiloa <125592066+michaelramoiloa@users.noreply.github.com>
2023-04-25 13:11:51 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -63,3 +63,18 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+/** 
+* print_number - prints an integer 
+* @n: integer to be printed *
+* Return: number of digits printed 
+
+*/
+
+int print_number(int n) { unsigned int m; int digits = 0; 
+if (n < 0) { _putchar('-'); m = -n; 
+}
+else m = n; 
+if (m / 10) digits += print_number(m / 10); 
+_putchar(m % 10 + '0'); 
+digits++; return (digits); 
+}
